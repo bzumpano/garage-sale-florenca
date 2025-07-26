fd1404a797e06b598be304372b34e8b2e703e595
Bruno Zumpano Nakashima Miyahara <zumpano.lp@gmail.com>
2025-07-25 23:59:23 -0300

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,6 @@ Rails.application.configure do
   config.cache_store = :solid_cache_store
 
   config.active_job.queue_adapter = :solid_queue
-  config.solid_queue.connects_to = { database: { writing: :queue } }
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
